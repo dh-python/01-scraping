7e32e0d92db9ff0d598ea79e72051403ec8ed0aa
Yohei Munesada <y.munesada@gmail.com>
2018-08-16 17:43:33 +0900

    スクレイピングの実装を追加

---
+++ scraping.py
@@ -0,0 +1,23 @@
+#
+# Pythonでスクレイピングするサンプルアプリです。
+# はてなブックマークの人気一覧を取得します。
+#
+# 取得元：
+#   http://b.hatena.ne.jp/hotentry/all
+#
+
+# 01. 必要なモジュールを読み込む.
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+# 02. HTMLを取得.
+with urlopen("http://b.hatena.ne.jp/hotentry/all") as response:
+    html = response.read().decode("utf-8")
+
+# 03. スクレイピング（タイトルの取得）
+soup = BeautifulSoup(html, "html.parser")
+
+h3_list = soup.select(".entrylist-contents-title")
+for h3 in h3_list:
+    title = h3.find("a").string
+    print(title)
